0019bf62b6654e4da918e856cd6847cf385c03e7
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-03-18 10:17:23 +0100

    first commit

---
+++ helloworld_universal/helloworld_universalViewController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace helloworld_universal
+{
+	public partial class helloworld_universalViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public helloworld_universalViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "helloworld_universalViewController_iPhone" : "helloworld_universalViewController_iPad", null)
+		{
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
